399a35b463393d87ee65e671bb53f0b28b4e761b
silvagov888-coder <silvagov888@gmail.com>
2026-08-24 18:08:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 
+const express = require('express');
+const { chromium } = require('playwright');
+
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+const GIRLS_URL = 'https://pt.pinterest.com/xtlnn0/moon-icons-girl/';
+const BOYS_URL = 'https://pt.pinterest.com/xtlnn0/moon-icons-boy/';
+
+// isPinImage()
+// normalizeUrl()
+// extractDomImages()
+// scrapeBoard()
+// scrapeBoysBoard()
+
+app.get('/scrape', async (req, res) => {
+    // executa Girls + Boys
+});
+
+app.get('/', (req, res) => {
+    res.send('Pinterest scraper online');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Pinterest scraper running on port ${PORT}`);
+});
